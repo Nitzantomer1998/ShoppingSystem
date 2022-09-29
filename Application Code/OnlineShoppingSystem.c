f3667144c13d84682731f2921694c58ef8cecbc4
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-09-29 15:25:16 +0300

    Adding copyString function body

--- Application Code/OnlineShoppingSystem.c
+++ Application Code/OnlineShoppingSystem.c
@@ -1,3 +1,14 @@
 #include "OnlineShoppingSystem.h"
 
 
+// Strings
+char* copyString(char* string)
+{
+    // Allocates the given string into a new string with initialized needed size of memory
+    char* memoryAllocateString = (char*) malloc(sizeof(char) * (strlen(string) + 1));
+    if (memoryAllocateString == NULL)
+        exit(true);
+
+    strcpy_s(memoryAllocateString, strlen(string) + 1, string);
+    return memoryAllocateString;
+}
