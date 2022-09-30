d7821cc2f0b47748123d391ff0bd2e2fe96b0f61
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-09-30 08:48:27 +0300

    Adding librarie stdio.h to work with scanf_s

---
+++ Application Code/ShoppingSystem.h
@@ -0,0 +1,18 @@
+#ifndef SHOPPINGSYSTEM_SHOPPINGSYSTEM_H
+#define SHOPPINGSYSTEM_SHOPPINGSYSTEM_H
+
+// Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// Enums
+typedef enum { false, true } bool;
+
+
+// Strings
+char* copyString(char* string);
+
+
+#endif //SHOPPINGSYSTEM_SHOPPINGSYSTEM_H
